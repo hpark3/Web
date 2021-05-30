f301fd78aa0db306535ad573c789d0ef02818e60
hpark3 <dalan-i@hanmail.net>
2021-05-30 17:25:51 +0900

    js: work/project filtering

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ navbarMenu.addEventListener('click', (event) => {
     scrollIntoView(link);
 });
 
-//Handle click on 'contact me' button on home
+//Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
@@ -41,6 +41,59 @@ document.addEventListener('scroll', () => {
     //console.log(1 - window.scrollY / homeHeight);
     home.style.opacity = 1 - window.scrollY / homeHeight;
 })
+
+//Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    }else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+//Handle click on "arrow up" button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+});
+
+//Work: Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    // console.log(filter);
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project);
+            if (filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300); // 0.3초가 지나면 우리가 등록한 animation을 다시 없앰
+             // opacity 0 -> 1 (원상복귀)
+
+    // console.log(`--------------`);
+    // for(let project of projects) {
+    //     console.log(project);
+    // }
+
+    // console.log(`--------------`);
+    // let project;
+    // for (let i = 0; i < projects.length; i++) {
+    //     project = projects[i];
+    //     console.log(project);
+    // }
+});
 
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
