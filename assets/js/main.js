e3dad0ba7b2d263570f99ece7937b252f6a323fd
hpark3 <dalan-i@hanmail.net>
2021-06-05 23:45:53 +0900

    final touch

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
-//Handle scrolling when tappint on the navbar menu
+//Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
@@ -34,6 +34,13 @@ const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 navbarToggleBtn.addEventListener('click', () => {
     navbarMenu.classList.toggle('open');
 });
+
+//Handle click on "Resume" on navbar
+const navbarMenuResume = document.getElementById('downloadLink');
+navbarMenuResume.addEventListener('click', function(event){
+    alert('Download my resume?');
+});
+
 
 //Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
